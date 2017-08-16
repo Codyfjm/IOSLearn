221660b232af147fa7d31a93c660d45d5fa24cff
cody <629523179@qq.com>
2017-08-16 21:52:35 +0800

    1.提交

---
+++ Tomcat/Tomcat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tomcat
+//
+//  Created by 冯俊铭 on 17/8/9.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
