5977ea2fc4f8710068388e49e0fa14ad4733c6ae
cody <629523179@qq.com>
2017-08-19 10:35:42 +0800

    8.19

---
+++ 02FirstPro/03FirstPro/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  03FirstPro
+//
+//  Created by 冯俊铭 on 17/8/19.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("H这事第二个程序!\n");
+    return 0;
+}
