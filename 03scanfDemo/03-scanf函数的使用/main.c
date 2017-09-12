ffecb6d9067e9e89876dd24eb59ebf8bda70c0ed
cody <629523179@qq.com>
2017-09-12 23:15:24 +0800

    iOS学习

---
+++ 03scanfDemo/03-scanf函数的使用/main.c
@@ -0,0 +1,38 @@
+//
+//  main.c
+//  03-scanf函数的使用
+//
+//  Created by 冯俊铭 on 17/9/12.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+//    
+//    float f1 = 0.0f;
+//    
+//    printf("请输入1个float数据：");
+//    scanf("%f",&f1);
+//    
+//    printf("你输入的数据是：%f\n",f1);
+    
+//    double d1 = 0.0;
+//    printf("请输入1个小数：");
+//    scanf("%lf",&d1);
+//    printf("");
+    
+    
+//    char ch = 'a';
+//    printf("请输入1个字符：");
+//    
+//    scanf("%c",&ch);
+//    
+//    printf("你输入的字符是：%c \n",ch);
+    
+    
+    
+    return 0;
+}
---
+++ 03scanfDemo/03scanfDemo/main.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  03scanfDemo
+//
+//  Created by 冯俊铭 on 17/9/12.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+  
+    
+//    int qqNumber = 0;
+//    int qqPwd = 0;
+//    
+//    printf("请输入QQ号码:");
+//    scanf("%d",&qqNumber);
+//    
+//     printf("请输入QQ密码:");
+//    scanf("%d",&qqPwd);
+//    
+//    printf("正在发送数据....\n");
+//    printf("恭喜你，%d 登录成功\n",qqNumber);
+    
+    
+    
+    
+//    int num = 0;
+//    int age = 0;
+//    printf("AAAAAAAAA\n");
+//    scanf("%d",&num);
+//    printf("num=%d\n",num);
+//    printf("BBBBBBBB\n");
+    
+//    
+//    int qqNumber = 123456;
+//    int qqPwd = 12345;
+//    
+//    printf("正在发送数据....\n");
+//    
+//    printf("恭喜你，%d 登录成功\n",qqNumber);
+    
+    return 0;
+}
