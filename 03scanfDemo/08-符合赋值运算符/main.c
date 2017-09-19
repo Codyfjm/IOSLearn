1f337585883fa24b6629fec53e9ca1135f2978b1
cody <629523179@qq.com>
2017-09-19 22:10:29 +0800

    9.19

---
+++ 03scanfDemo/08-符合赋值运算符/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  08-符合赋值运算符
+//
+//  Created by 冯俊铭 on 17/9/19.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  
+
+    int num = 10;
+    num = num + 2;
+    
+    printf("num = %d\n",num);
+    
+}
