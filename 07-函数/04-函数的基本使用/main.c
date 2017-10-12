7b146c77010bdb21507161383595e65f4d663dd2
cody <629523179@qq.com>
2017-10-13 00:23:54 +0800

    1.函数调试

---
+++ 07-函数/04-函数的基本使用/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  04-函数的基本使用
+//
+//  Created by 冯俊铭 on 17/10/12.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+/**
+ *这个函数是来显示游戏的标志的
+ */
+/**
+ *  <#Description#>
+ */
+void showLogo(){
+    printf("===========================\n");
+    printf("=                         =\n");
+    printf("=    不好玩的推箱子游戏      =\n");
+    printf("=                         =\n");
+    printf("===========================\n");
+}
+
+int main(int argc, const char * argv[]) {
+    showLogo();
+}
