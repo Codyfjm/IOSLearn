e8d217a7c78ded9416f1717cb232761bbfa9383f
cody <629523179@qq.com>
2017-10-14 00:08:07 +0800

    预处理

---
+++ 08-进制/01-预处理/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  01-预处理
+//
+//  Created by 冯俊铭 on 17/10/13.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[]) {
+   #include "/Users/codyfjm/Desktop/1.txt"
+    return 0;
+}
---
+++ 08-进制/01-预处理概述/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  01-预处理概述
+//
+//  Created by 冯俊铭 on 17/10/13.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+/**
+ 预处理代码：以＃开头的代码
+ 分类：
+ 1.文件包含指令：#include
+ 2.宏定义 #define
+ 3.条件编译指令 #if
+ 
+ **/
+
+#include <stdio.h>
+#include "/Users/codyfjm/Desktop/1.rtf"
+
+int main(int argc, const char * argv[]) {
+
+    
+}
