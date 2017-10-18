b5889cb6f81ce720f579803b64f08836ed30c730
cody <629523179@qq.com>
2017-10-18 23:06:55 +0800

    进制

---
+++ 08-进制/04-进制/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  04-进制
+//
+//  Created by 冯俊铭 on 17/10/18.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+//    int num = 10;
+//    if ((num & 1) == 0) {
+//        printf("偶数\n");
+//    } else {
+//        printf("不是1个偶数\n");
+//    }
+    
+    
+    
+    
+}
