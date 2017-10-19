cfeb025f189b2e3abbc86af7214de64430481422
cody <629523179@qq.com>
2017-10-19 23:49:01 +0800

    1.进制－－变量

---
+++ 08-进制/09-深入变量细节/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  09-深入变量细节
+//
+//  Created by 冯俊铭 on 17/10/19.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int num1 = 20;
+    int num2 = 10;
+    printf("num1的地址是：%p\n",&num1);
+    printf("num1的地址是：%p\n",&num2);
+}
