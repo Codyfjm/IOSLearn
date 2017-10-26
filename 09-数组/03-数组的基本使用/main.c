70883bc91f9001378d4e159f4a2f62b3043d858a
cody <629523179@qq.com>
2017-10-26 23:49:41 +0800

    1.数组

---
+++ 09-数组/03-数组的基本使用/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  03-数组的基本使用
+//
+//  Created by 冯俊铭 on 17/10/25.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+#define LEN 10
+
+int main(int argc, const char * argv[]) {
+    
+    int arr[3];
+    int arr2[LEN];
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    
+    for (int i=0; i<3; i++) {
+        printf("%d\n",arr[i]);
+    }
+    
+    
+}
---
+++ 09-数组/04-数组的概述/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  04-数组的概述
+//
+//  Created by 冯俊铭 on 17/10/24.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+
+    
+}
