da11fa6a0c38053f745a3d2afa2768905f7b4fda
cody <629523179@qq.com>
2017-10-27 00:20:26 +0800

    数组2

--- 09-数组/03-数组的基本使用/main.c
+++ 09-数组/03-数组的基本使用/main.c
@@ -7,15 +7,18 @@
 //
 
 #include <stdio.h>
-#define LEN 10
+//#define LEN 10
 
 int main(int argc, const char * argv[]) {
     
-    int arr[3];
-    int arr2[LEN];
-    arr[0] = 10;
-    arr[1] = 20;
-    arr[2] = 30;
+    
+    int arr[3] = {10,20,30};
+    
+    
+//    int arr2[LEN];
+//    arr[0] = 10;
+//    arr[1] = 20;
+//    arr[2] = 30;
     
     for (int i=0; i<3; i++) {
         printf("%d\n",arr[i]);
