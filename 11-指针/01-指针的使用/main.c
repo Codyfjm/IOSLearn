ed976fcf3a2a6a45859532744fdc42817a76b187
cody <629523179@qq.com>
2017-12-14 23:05:35 +0800

    1.指针的使用

--- 11-指针/01-指针的使用/main.c
+++ 11-指针/01-指针的使用/main.c
@@ -22,11 +22,53 @@
     这个指针变量的类型是int* 读作int指针
     这个p1指针变量中只能存储地址
  
+ 5.正确的初始化
+    a.先取出变量的地址
+ 
+    b.将取出来的变量的地址赋值给指针变量
+ 
+ 6.野指针：未初始化的指针
+ 
+ 7.NULL值：代表指针变量不指向内存中的任何地址，完全＝＝＝0
+ 
  */
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    
+//    int num = 10;
+//    int* p1 = &num;
+    
+//    int age = 10;
+//    int* p1 = &age;
+//    
+//    //p1的值，即是age的地址
+//    printf("p1的值 = %p\n",p1);
+//    printf("age的地址：%p\n",&age);
+//    
+//    //p1指针变量的地址
+//    printf("p1指针变量的地址是：%p\n",&p1);
+    
+    
+//    int num = 10;
+//    int* p1 = &num;
+//    
+//    *p1 = 500;
+//    //*p1 代表p1指针指向的变量
+//    
+//    printf("num = %d\n",num);
+//    
+//    printf("指针指向的变量:%d\n",*p1);
+    
+    int num = 10;
+    
+    int* p1 = &num;
+    int* p2 = p1;
+    
+    *p1 = 100;
+    
+    printf("p2:%d\n",*p2);//100
     
     
 }
