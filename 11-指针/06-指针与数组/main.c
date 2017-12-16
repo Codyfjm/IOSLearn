e48fd9a708acda323215ae2b7ea9fcc2bfab4646
cody <629523179@qq.com>
2017-12-16 21:12:47 +0800

    指针和数组

---
+++ 11-指针/06-指针与数组/main.c
@@ -0,0 +1,48 @@
+//
+//  main.c
+//  06-指针与数组
+//
+//  Created by 冯俊铭 on 17/12/16.
+//  Copyright © 2017年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int arr[7] = {10,20,30,40,50,60,70};
+    
+    
+    int* p1 = arr;
+    
+    
+    
+    
+    
+//    for (int i=0; i<7; i++) {
+//        printf("%d\n",*(p1+i));
+//        printf("%d\n",*(arr+i));
+//        printf("%d\n",*(p1++));
+//    }
+    
+//    
+//    int* p1 = &arr[0];
+//    int* p2 = arr;
+//    
+//    printf("p1 = %p\n",p1);
+//    printf("p2 = %p\n",p2);
+    
+    
+//    int* p0 = &arr[0];
+//    int* p1 = &arr[1];
+//    int* p2 = &arr[2];
+//    
+//    *p0 = 100;
+//    *p1 = 200;
+//    *p2 = 300;
+//    
+//    for (int i=0; i<3; i++) {
+//        printf("%d\n",arr[i]);
+//    }
+    
+}
