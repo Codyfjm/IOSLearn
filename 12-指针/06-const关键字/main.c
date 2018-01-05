b2e98557600e8b308a6baaaeaf0ca9a381e11601
cody <629523179@qq.com>
2018-01-05 23:03:53 +0800

    const关键字使用

---
+++ 12-指针/06-const关键字/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  06-const关键字
+//
+//  Created by 冯俊铭 on 18/1/5.
+//  Copyright © 2018年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    
+  
+//    //num的值只能取值，不允许修改
+//    const int num = 10;
+//    
+//    //数组的元素不能更改
+//    const int arr[4] = {10,20,30,40};
+//    
+//    //无法通过p1指针去修改指针指向的变量的值
+//    int num = 10;
+//    const int* p1 = &num;
+//    
+//    //p1的值不能修改，但是可以通过p1去修改p1指向的变量的值
+//    int * const p1 = &num;
+    
+    
+}
