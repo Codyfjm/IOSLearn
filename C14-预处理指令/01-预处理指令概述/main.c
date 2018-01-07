082830a8b81649e8cbc532499d4f40960c3f456d
cody <629523179@qq.com>
2018-01-07 15:41:38 +0800

    预处理指令概述

---
+++ C14-预处理指令/01-预处理指令概述/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  01-预处理指令概述
+//
+//  Created by 冯俊铭 on 18/1/7.
+//  Copyright © 2018年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ C14-预处理指令/C14-预处理指令/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  C14-预处理指令
+//
+//  Created by 冯俊铭 on 18/1/7.
+//  Copyright © 2018年 cody. All rights reserved.
+//
+/*
+ 
+    1，预处理指令的特点
+        1）＃开头
+        2）编译之前执行
+        3）后面没分号
+ 
+    2，c程序从编写、编译、链接、执行的流程
+        1）新建1个.c的源文件
+        2）在。c文件写c代码
+        3）使用： cc -c 源文件名称指令变异源文件
+        4）链接
+            cc xx.o－－》.out文件
+ 
+    3，分类
+        1）文件包含指令 ＃include
+        2）宏定义：＃define
+        3）条件编译指令
+ 
+ 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+}
