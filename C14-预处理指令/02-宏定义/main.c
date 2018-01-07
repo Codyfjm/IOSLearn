aeceed871d4dd1b89f421e935edb541b64d84b67
cody <629523179@qq.com>
2018-01-07 19:30:00 +0800

    宏定义

---
+++ C14-预处理指令/02-宏定义/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  02-宏定义
+//
+//  Created by 冯俊铭 on 18/1/7.
+//  Copyright © 2018年 cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    //#define 是1个预处理指令，在预编译的时候会把宏名换成宏值
+    //typedef 是1个c代码，在运行的时候才会执行
+}
