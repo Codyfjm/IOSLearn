b0520585ced71b809a3882b8df72b29789c5f0ce
cody <629523179@qq.com>
2018-01-15 21:53:01 +0800

    结构体的初始化

--- 13-结构体和枚举/03-结构体的基本使用/main.c
+++ 13-结构体和枚举/03-结构体的基本使用/main.c
@@ -16,6 +16,9 @@
  
  声明结构体类型的变量
     struct 新类型名称 变量名;
+ 
+ 结构体变量的初始化
+    结构体变量名.成员名 ＝ 数据;
  */
 
 #include <stdio.h>
@@ -30,4 +33,14 @@ int main(int argc, const char * argv[]) {
     };
     
     struct Student stu;//代表一个学生（对象）
+    stu.name = "jack";
+    stu.age = 18;
+    stu.score = 100;
+    stu.height = 189.5;
+    
+    printf("名字:%s，年龄:%d，成绩:%d，身高:%2f\n",
+           stu.name,
+           stu.age,
+           stu.score,
+           stu.height);
 }
